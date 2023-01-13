30b574c635f5b8a41af05420da20f13e74a05588
Emmanuel <manuboisset@gmail.com>
2023-01-13 20:29:12 +0100

    ajout page mes likes et ma wishlist

--- app/src/main/java/com/example/gamepod/MainActivity.kt
+++ app/src/main/java/com/example/gamepod/MainActivity.kt
@@ -1,15 +1,33 @@
 package com.example.gamepod
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home)
+
+
+        val myLikes = findViewById<ImageView>(R.id.to_my_fav)
+        val myWishList = findViewById<ImageView>(R.id.to_my_wish_list)
+
+        myLikes.setOnClickListener{
+            val toMyLikes = Intent(this, MyLikes::class.java)
+            startActivity(toMyLikes)
+        }
+
+        myWishList.setOnClickListener {
+            val toMyWishList = Intent(this, MyWishList::class.java)
+            startActivity(toMyWishList)
+        }
+
+
     }
-
-
 
 
 }
