3779787f932ffd74ef365f2d42cc8749b457efdd
Emmanuel <manuboisset@gmail.com>
2023-01-15 23:12:52 +0100

    Ajout Layout reviews

---
+++ app/src/main/java/com/example/gamepod/detailsGame.kt
@@ -0,0 +1,11 @@
+package com.example.gamepod
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class detailsGame : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details_game)
+    }
+}
