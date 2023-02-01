f67a76f7ffbb85e5cd196c81afc41a8b9d82a856
noeoxycode <76428537+noeoxycode@users.noreply.github.com>
2023-02-02 00:13:50 +0100

    GameDetails on the way

---
+++ app/src/main/java/com/example/gamepod/gameDetails/GameDetailsActivity.kt
@@ -0,0 +1,18 @@
+package com.example.gamepod.gameDetails
+
+import android.os.Bundle
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+import com.example.gamepod.R
+
+class GameDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.container)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, ******.newInstance()).commit()
+    }
+
+}
