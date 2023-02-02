80b2cea40e21a2d5face8c4dd61ba081faedf8e7
noe <noe@marleix.com>
2023-02-02 11:13:33 +0100

    forgotpassword done

---
+++ app/src/main/java/com/example/gamepod/forgotPassword/ForgotPasswordActivity.kt
@@ -0,0 +1,20 @@
+package com.example.gamepod.forgotPassword
+
+import android.os.Bundle
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.gamepod.R
+import com.example.gamepod.connexion.ConnexionFragment
+
+class ForgotPasswordActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.container)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, ForgotPasswordFragment.newInstance()).commit()
+    }
+
+}
